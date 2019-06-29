6942d107d31cb14a4fbec891df8f9d7acce4d0d2
Tom Dobbelaere <tom.dobbelaere@outlook.com>
2019-06-29 15:00:54 +0200

    Service example

--- src/components/lib/reaction-details/reaction-details.test.tsx
+++ src/components/lib/reaction-details/reaction-details.test.tsx
@@ -8,14 +8,14 @@ describe("when the component loads", () => {
   });
 
   it("should use the singular form if the count is 1", () => {
-    const textContent = "1 person is okay with this";
+    const textContent = "1 person likes this";
     const { container } = render(<ReactionDetails count={1} />);
 
     expect(container).toHaveTextContent(textContent);
   });
 
   it("should use the plural form if the count is not 1", () => {
-    const textContent = "2 people are okay with this";
+    const textContent = "2 people like this";
     const { container } = render(<ReactionDetails count={2} />);
 
     expect(container).toHaveTextContent(textContent);
---
+++ src/services/status-update.service.ts
@@ -0,0 +1,11 @@
+class StatusUpdateService {
+  constructor() {
+    console.log("am created");
+  }
+
+  public test() {}
+}
+
+let instance = new StatusUpdateService();
+
+export default instance;
